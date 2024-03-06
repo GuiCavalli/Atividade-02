a0e8b69ee8e01dceabdf5e27f1b8927f4b8f1f8d
GuiCavalli <160363168+GuiCavalli@users.noreply.github.com>
2024-03-05 21:59:52 -0300

    atividade01.java

---
+++ atividade01.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        String nome = "";
+        int idade = 0;
+        double peso = 0.0;
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Informe seu nome:  ");
+        nome = scanner.nextLine();
+        System.out.println("Informe sua idade:  ");
+        idade = scanner.nextInt();
+        
+        System.out.println("Informe seu peso:  ");
+        peso = scanner.nextDouble();
+        
+        scanner.close();
+        
+        System.out.println("Nome "+ nome);
+        System.out.println("idade "+ idade);
+        System.out.println("peso "+ peso);
+        
+    
+    }
+}
